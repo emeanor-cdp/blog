5279d7fc802cfc9706b0da1da187f05c3ededff7
Ethan Meanor <emeanor.cdp@gmail.com>
2021-01-08 11:48:46 -0500

    Modify routes.rb and the index view to use the resources helper methods.

--- config/routes.rb
+++ config/routes.rb
@@ -3,12 +3,18 @@ Rails.application.routes.draw do
   # ArticleController.
   root 'articles#index'
 
+  # Creates all the routes needed for a basic CRUD object, so we don't have to
+  # do it manually.
+  resources :articles
+  
+  
+  ### Below is the manual way that's obscured by the simpler code above.
   # This route declares that GET /articles requests are mapped to the index
   # action of ArticlesController.
-  get '/articles', to: 'articles#index'
+  #get '/articles', to: 'articles#index'
 
   # :id designates a route parameter. A route parameter captures a segment of 
   # the request's path, and puts that value into the params Hash, which is 
   # accessible by the controller action (in this case, show).
-  get '/articles/:id', to: 'articles#show'
+  #get '/articles/:id', to: 'articles#show'
 end
