5d703c083ac2c931b51f741ed1be025d4869e1e8
Ethan Meanor <emeanor.cdp@gmail.com>
2021-01-08 11:06:41 -0500

    Add a route to display a single article, and add a show action to ArticlesController for the same purpose.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,8 @@ class ArticlesController < ApplicationController
     # Controller instance variables can be accessed by the view.
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,9 @@ Rails.application.routes.draw do
   # This route declares that GET /articles requests are mapped to the index
   # action of ArticlesController.
   get '/articles', to: 'articles#index'
+
+  # :id designates a route parameter. A route parameter captures a segment of 
+  # the request's path, and puts that value into the params Hash, which is 
+  # accessible by the controller action (in this case, show).
+  get '/articles/:id', to: 'article#show'
 end
