1e134ace1f79cd977b6619fc2c3c09eeb521b2c5
Ethan Meanor <emeanor.cdp@gmail.com>
2021-01-08 14:47:24 -0500

    Add comments section to article show view.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@ class CommentsController < ApplicationController
   def create
     # Have to keep track of the article to which the comment is attached.
     @article = Article.find(params[:article_id])
+    # Create the comment via its article, so they are automatically associated.
     @comment = @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
