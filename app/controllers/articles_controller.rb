82ff54cfcf8d8d4289aeb397e47c954438dcb2e3
Ethan Meanor <emeanor.cdp@gmail.com>
2021-01-08 12:01:58 -0500

    Add #new and #create methods to ArticlesController.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,4 +9,23 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+
+  def new
+    # Instantiates a new article but doesn't save it. The view contains the form
+    # to create a new article.
+    @article = Article.new
+  end
+
+  def create
+    # Instantiates a new article with values for title and body, passed from the
+    # article creation form in the #new view.
+    @article = Article.new(title: '...', body: '...')
+
+    # Attempt to save it. Redirect to its page if successful.
+    if @article.save
+      redirect_to article
+    else
+      # If not successful, redisplay the creation form in the #new view.
+      render :new
+  end
 end
