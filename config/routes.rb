83a6892ecb9dbcc43a48fb2a0f44ae066e153fec
Ethan Meanor <emeanor.cdp@gmail.com>
2021-01-08 10:20:26 -0500

    Creates a root route to map the default page to the index action of ArticleController.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # This route declares that GET / requests are mapped to the index action of
+  # ArticleController.
+  root 'articles#index'
+
   # This route declares that GET /articles requests are mapped to the index
   # action of ArticlesController.
   get '/articles', to: 'articles#index'
